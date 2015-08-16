7aea0592499815c293d15edf5d8d0a542a4acc49
= <isvaldo.fernandes@gmail.com>
2015-08-16 17:21:02 -0300

    first commit

---
+++ src/br/projeto/home/Home.java
@@ -0,0 +1,67 @@
+package br.projeto.home;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.projeto.modelo.Pessoa;
+
+/**
+ * Servlet implementation class Home
+ */
+@WebServlet("/Home")
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Home() {
+        super();
+        // TODO Auto-generated constructor stub
+        
+        
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		Pessoa pessoa = new Pessoa();
+		pessoa.setAgencia("201");
+		pessoa.setTitular("Pedro");
+		pessoa.setBanco("Real");
+		
+		EntityManagerFactory entityManagerFactory = Persistence
+				.createEntityManagerFactory("banco");
+
+		EntityManager manager = entityManagerFactory.createEntityManager();
+		
+		manager.getTransaction().begin();
+		manager.persist(pessoa);
+		manager.getTransaction().commit();
+		
+
+  manager.close();
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
